acf3f3e986ff1b454c4cc4c5992ae3794423a994
air.ye <air.ye@tron.network>
2023-03-14 19:31:18 +0800

    update case

--- mirror/src/test/java/org/tron/defi/contract_mirror/config/ContractConfigListTest.java
+++ mirror/src/test/java/org/tron/defi/contract_mirror/config/ContractConfigListTest.java
@@ -23,6 +23,6 @@ public class ContractConfigListTest {
         Assertions.assertNotNull(contractConfigList);
         List<ContractConfigList.ContractConfig> configList = contractConfigList.getContracts();
         log.info(configList.toString());
-        Assertions.assertEquals(9, configList.size());
+        Assertions.assertEquals(10, configList.size());
     }
 }
