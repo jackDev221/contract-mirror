2a7576446b21c1dd381a0bb3c646e55b3e1e2db1
air.ye <air.ye@tron.network>
2023-03-18 16:07:25 +0000

    test diff.js

--- tools/diff.mjs
+++ tools/diff.mjs
@@ -2,50 +2,60 @@ import fetch from 'node-fetch';
 // import { initTronWeb } from './tron.mjs';
 import config from './config.js';
 const { mainnet, nile } = config;
-// import utils from 'web3-utils';
-// const { toBN, toWei } = utils;
+import utils from 'web3-utils';
+const { toBN, toWei } = utils;
 
 const cfg = mainnet;
 // const tronWeb = await initTronWeb(cfg);
 
 let tokenList = new Map();
-tokenList.set('TRX', '9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb');
-tokenList.set('USDC', 'TWMCMCoJPqCGw5RR7eChF2HoY3a9B8eYA3');
-tokenList.set('SUN', 'TDqjTkZ63yHB19w2n7vPm2qAkLHwn9fKKk');
-tokenList.set('DICE', 'TLVu3Pzaep38SGvgCxUe1cUk2SNM6fyR4e');
-tokenList.set('LIVE', 'TLwpNV2gVkVk1g7ejZJ2hqzsE7RZvDuRSb');
-tokenList.set('QUINCE', 'TT8vc3zKCmGCUryYWLtFvu5PqAyYoZ3KMh');
-tokenList.set('SUNOLD', 'TWrZRHY9aKQZcyjpovdH6qeCEyYZrRQDZt');
-tokenList.set('BTC', 'TG9XJ75ZWcUw69W8xViEJZQ365fRupGkFP');
-tokenList.set('WBTC', 'TN3W4H6rK2ce4vX9YnFQHwKENnHjoxb3m9');
-tokenList.set('ETH', 'TQz9i4JygMCzizdVu8NE4BdqesrsHv1L93');
-tokenList.set('WBTT', 'TLELxLrgD3dq6kqS4x6dEGJ7xNFMbzK95U');
-tokenList.set('WTRX', 'TYsbWxNnyTgsZaTFaue9hqpxkU3Fkco94a');
-tokenList.set('JST', 'TF17BgPaZYbz8oxbjhriubPDsA7ArKoLX3');
-tokenList.set('WIN', 'TNDSHKGBmgRx9mDYA9CnxPx55nu672yQw2');
-tokenList.set('USDT', 'TXYZopYRdj2D9XRtbG411XZZ3kM5VkAeBf');
-tokenList.set('USDJ', 'TLBaRhANQoJFTqre9Nf1mjuwNWjCJeYqUL');
-tokenList.set('TUSD', 'TRz7J6dD2QWxBoumfYt4b3FaiRG23pXfop');
-tokenList.set('HT', 'TGfVzt44kg6ZJ4fUqpHzJy3Jb37YMf8pMH');
-tokenList.set('USDDPSM', 'TWvPn9LWmNd1DMtt52yKHhNJazi7sDfcUq');
-tokenList.set('USDD', 'TGjgvdTWWrybVLaVeFqSyVqJQWjxqRYbaK');
+tokenList.set('TRX', {address:'T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb', decimals: 6});
+tokenList.set('SUN', {address:'TD3et9gS2pYz46ZC2mkCfYcKQGNwrnBLef', decimals: 18});
+tokenList.set('SUNOLD', {address: 'TKkeiboTkxXKJpbmVFbv4a8ov5rAfRDMf9', decimals: 18});
+tokenList.set('BTC', {address: 'TN3W4H6rK2ce4vX9YnFQHwKENnHjoxb3m9', decimals: 8});
+tokenList.set('WBTC', {address:'TXpw8XeWYeTUd4quDskoUqeQPowRh4jY65', decimals: 8});
+tokenList.set('ETH', {address:'THb4CqiFdwNHsWsQCs4JhzwjMWys4aqCbF', decimals:18});
+tokenList.set('WETH', {address:'TXWkP3jLBqRGojUih1ShzNyDaN5Csnebok',decimals:18});
+tokenList.set('WBTT', {address:'TKfjV9RNKJJCqPvBtK8L7Knykh7DNWvnYt', decimals:6});
+tokenList.set('WTRX', {address:'TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR',decimals:6});
+tokenList.set('JST', {address:'TCFLL5dx5ZJdKnWuesXxi1VPwjLVmWZZy9', decimals:18});
+tokenList.set('WIN', {address:'TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7',decimals:6});
+tokenList.set('DICE', {address:'TKttnV3FSY1iEoAwB4N52WK2DxdV94KpSd',decimals:6});
+tokenList.set('LIVE', {address:'TVgAYofpQku5G4zenXnvxhbZxpzzrk8WVK',decimals:6});
+tokenList.set('LTC', {address:'TR3DLthpnDdCGabhVDbD3VMsiJoCXY3bZd',decimals:8});
+tokenList.set('HT', {address:'TDyvndWuvX5xTBwHPYJi7J3Yq8pq8yh62h',decimals:18});
+tokenList.set('USDT', {address:'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t',decimals:6});
+tokenList.set('USDJ', {address:'TMwFHYXLJaRUPeW6421aqXL4ZEzPRFGkGT',decimals:18});
+tokenList.set('TUSD', {address:'TUpMhErZL2fhh4sVNULAbNKLokS4GjC1F4',decimals:18});
+tokenList.set('USDC', {address:'TEkxiTehnzSmSe2XqrBj4w32RUN966rdz8',decimals:6});
+tokenList.set('USDD', {address:'TPYmHEhy5n8TCEfYGqW2rPxsghSfzghPDn',decimals:18});
 
 let paths = new Set();
 let total = 0;
 let success = 0;
+let maxTime = 0;
+let minTime = 999999999999;
+let totalTime = 0;
 
 async function diffPair(from, to) {
-  let url = `http://${cfg.routerServer}/swap/router?fromToken=${from}&toToken=${to}&amountIn=1000000`;
+  let amountIn = toBN(2).mul(toBN(10).pow(toBN(from.decimals))).toString();
+  let url = `http://${cfg.routerServer}/swap/router?fromToken=${from.address}&toToken=${to.address}&amountIn=${amountIn}`;
   console.log(url);
   try {
     total = total + 1;
+    let t0 = Date.now();
     let response = await fetch(url);
+    let t1 = Date.now();
+    let t = t1 - t0;
+    maxTime = Math.max(maxTime, t);
+    minTime = Math.min(minTime, t);
+    totalTime += t;
     response = await response.json();
-    if (response[code] != 0) {
+    console.log(response);
+    if (response.code != 0) {
       console.log(response);
       return 0;
     }
-    console.log(response);
     success = success + 1;
     return 0;
   } catch (e) {
@@ -54,14 +64,15 @@ async function diffPair(from, to) {
   }
 }
 
-for (let [name0, address0] of tokenList) {
-  for (let [name1, address1] of tokenList) {
-    if (address0 == address1) {
+for (let [name0, token0] of tokenList) {
+  for (let [name1, token1] of tokenList) {
+    if (name0 == name1) {
       continue;
     }
     let count = 0;
     do {
-      count = await diffPair(address0, address1);
+      count = await diffPair(token0, token1);
     } while (count > 0);
   }
 }
+console.log(`total=${total} success=${success} maxTime=${maxTime} minTime=${minTime} avgTime=`, totalTime/total);
