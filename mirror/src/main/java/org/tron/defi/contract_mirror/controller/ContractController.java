f0f89955fa546fd48b64fb3923853375b96828fd
air.ye <air.ye@tron.network>
2023-03-22 13:13:04 +0800

    update

--- mirror/src/main/java/org/tron/defi/contract_mirror/controller/ContractController.java
+++ mirror/src/main/java/org/tron/defi/contract_mirror/controller/ContractController.java
@@ -1,20 +1,19 @@
 package org.tron.defi.contract_mirror.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.tron.defi.contract_mirror.dto.Response;
 import org.tron.defi.contract_mirror.service.ContractService;
 
-@Controller
+@RestController
 public class ContractController {
     @Autowired
     ContractService contractService;
 
     @RequestMapping("/contract/{address}/{method}")
-    @ResponseBody
     public Response callContract(@PathVariable String address, @PathVariable String method) {
         Response response = new Response();
         try {
@@ -27,7 +26,7 @@ public class ContractController {
         return response;
     }
 
-    @RequestMapping("/info/{address}")
+    @GetMapping("/info/{address}")
     public Response contractInfo(@PathVariable String address) {
         Response response = new Response();
         try {
