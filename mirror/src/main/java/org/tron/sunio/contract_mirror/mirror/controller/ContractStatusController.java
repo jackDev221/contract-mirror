981b7b33c18878a1cf14f0c95a82cfc74203fdcd
lvbin <lvbin@hnnewsoft.com>
2023-03-27 11:21:28 +0800

    feature: url add pre 'swap'

--- mirror/src/main/java/org/tron/sunio/contract_mirror/mirror/controller/ContractStatusController.java
+++ mirror/src/main/java/org/tron/sunio/contract_mirror/mirror/controller/ContractStatusController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.tron.sunio.contract_mirror.mirror.contracts.BaseContract;
@@ -31,6 +32,7 @@ import static org.tron.sunio.contract_mirror.mirror.consts.ContractMirrorConst.M
 
 @RestController
 @Slf4j
+@RequestMapping("/swap/")
 public class ContractStatusController {
     @Autowired
     private ContractMirror contractMirror;
