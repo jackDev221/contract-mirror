a4b9ad73807f37fbec714e91e07ea3b28468967a
air.ye <air.ye@tron.network>
2023-03-30 20:48:35 +0800

    update

--- mirror/src/main/java/org/tron/defi/contract_mirror/service/RouterService.java
+++ mirror/src/main/java/org/tron/defi/contract_mirror/service/RouterService.java
@@ -37,6 +37,7 @@ public class RouterService {
     public RouterService(MeterRegistry meterRegistry) {
         candidateNum = DistributionSummary.builder("num_router_candidates")
                                           .description("Number of path candidates")
+                                          .publishPercentileHistogram()
                                           .publishPercentiles(0.5, 0.8, 0.99)
                                           .register(meterRegistry);
         for (String strategy : StrategyFactory.getInstance().getStrategyNames()) {
@@ -44,6 +45,7 @@ public class RouterService {
                                  DistributionSummary.builder("strategy_duration_" + strategy)
                                                     .description("Time token for strategy")
                                                     .baseUnit("ms")
+                                                    .publishPercentileHistogram()
                                                     .publishPercentiles(0.5, 0.95, 0.99, 0.9999)
                                                     .percentilePrecision(2)
                                                     .maximumExpectedValue(Duration.ofSeconds(10)
